c57005aaf451ae038f87c38667acf790946105bb
Ewelina Sitarczyk <aivellyn8@gmail.com>
2023-03-13 21:19:55 +0100

    Custom admins for event and location

--- website/events/admin.py
+++ website/events/admin.py
@@ -2,5 +2,31 @@ from django.contrib import admin
 
 from .models import Event, Location
 
-admin.site.register(Event)
-admin.site.register(Location)
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "latitude",
+        "longitude",
+        "country",
+        "city",
+        "street",
+        "street_number",
+        "zip_code",
+    )
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "event_type",
+        "status",
+        "start_time",
+        "end_time",
+        "location",
+    )
+    list_filter = ("event_type", "status")
+
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Event, EventAdmin)
